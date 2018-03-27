649cc411f2321f9983e801c8898989d2283dbc60
Gerald Bauer <gerald.bauer@gmail.com>
2018-03-27 17:24:34 +0200

    Create SiringClockAuction.sol

---
+++ contracts/SiringClockAuction.sol
@@ -0,0 +1,65 @@
+/// @title Reverse auction modified for siring
+/// @notice We omit a fallback function to prevent accidental sends to this contract.
+
+contract SiringClockAuction is ClockAuction {
+
+    // @dev Sanity check that allows us to ensure that we are pointing to the
+    //  right auction in our setSiringAuctionAddress() call.
+    bool public isSiringClockAuction = true;
+
+    // Delegate constructor
+    function SiringClockAuction(address _nftAddr, uint256 _cut) public
+        ClockAuction(_nftAddr, _cut) {}
+
+    /// @dev Creates and begins a new auction. Since this function is wrapped,
+    /// require sender to be KittyCore contract.
+    /// @param _tokenId - ID of token to auction, sender must be owner.
+    /// @param _startingPrice - Price of item (in wei) at beginning of auction.
+    /// @param _endingPrice - Price of item (in wei) at end of auction.
+    /// @param _duration - Length of auction (in seconds).
+    /// @param _seller - Seller, if not the message sender
+    function createAuction(
+        uint256 _tokenId,
+        uint256 _startingPrice,
+        uint256 _endingPrice,
+        uint256 _duration,
+        address _seller
+    )
+        external
+    {
+        // Sanity check that no inputs overflow how many bits we've allocated
+        // to store them in the auction struct.
+        require(_startingPrice == uint256(uint128(_startingPrice)));
+        require(_endingPrice == uint256(uint128(_endingPrice)));
+        require(_duration == uint256(uint64(_duration)));
+
+        require(msg.sender == address(nonFungibleContract));
+        _escrow(_seller, _tokenId);
+        Auction memory auction = Auction(
+            _seller,
+            uint128(_startingPrice),
+            uint128(_endingPrice),
+            uint64(_duration),
+            uint64(now)
+        );
+        _addAuction(_tokenId, auction);
+    }
+
+    /// @dev Places a bid for siring. Requires the sender
+    /// is the KittyCore contract because all bid methods
+    /// should be wrapped. Also returns the kitty to the
+    /// seller rather than the winner.
+    function bid(uint256 _tokenId)
+        external
+        payable
+    {
+        require(msg.sender == address(nonFungibleContract));
+        address seller = tokenIdToAuction[_tokenId].seller;
+        // _bid checks that token ID is valid and will throw if bid fails
+        _bid(_tokenId, msg.value);
+        // We transfer the kitty back to the seller, the winner will get
+        // the offspring
+        _transfer(seller, _tokenId);
+    }
+
+}
